0ab8fd06320b64f3c6df091bede39e1e9bb2f49d
DJacquenson <doinejacquenson@gmail.com>
2020-03-09 11:43:23 -0400

    Creating the first app.

---
+++ app/src/main/java/com/example/simpletodo/ItemsAdapter.java
@@ -0,0 +1,77 @@
+package com.example.simpletodo;
+
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class ItemsAdapter extends RecyclerView.Adapter<ItemsAdapter.ViewHolder> {
+
+
+
+    //@NonNull
+    public interface OnLongClickListener{
+        void onItemLongClicked(int position);
+    }
+    List<String> items;
+    OnLongClickListener longClickListener;
+    public ItemsAdapter(List<String> items ,OnLongClickListener longClickListener){
+        this.items = items;
+        this.longClickListener=longClickListener;
+    }
+
+
+
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        //Usse layout inflater to inflate to view
+        View todoView = LayoutInflater.from(parent.getContext()).inflate(android.R.layout.simple_list_item_1,parent, false);
+        // wrap it inside a view Holder and return it
+         return new ViewHolder(todoView);
+    }
+
+
+    @Override
+    public void onBindViewHolder(@NonNull ItemsAdapter.ViewHolder holder, int position) {
+        // Grab the item at the position
+        String item = items.get(position);
+        //Bind the item into the specied view holder
+        holder.bind(item);
+    }
+
+    @Override
+    public int getItemCount() {
+
+        return items.size();
+    }
+
+    // Container to provide easy access to view List
+    class ViewHolder extends RecyclerView.ViewHolder {
+
+        TextView tvItem;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            tvItem = itemView.findViewById(android.R.id.text1);
+        }
+
+        public void bind(String item) {
+            tvItem.setText(item);
+            tvItem.setOnLongClickListener(new View.OnLongClickListener() {
+                @Override
+                public boolean onLongClick(View v) {
+                    // Remove the item from the recycler View
+                    longClickListener.onItemLongClicked(getAdapterPosition());
+                    //OnLongClickListener.onItemLongClicked(getAdapterPosition());
+                    return false;
+                }
+            });
+        }
+    }
+}
